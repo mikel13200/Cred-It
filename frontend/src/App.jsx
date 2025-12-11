02201da25ed2d140213dc39abaf77e0db283aaa5
mikel13200 <mikel.dadula03@gmail.com>
2025-12-11 17:07:18 +0800

    Remove integrate google and github (1)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import { HomePage as StudentHome, StudentDocumentPage } from './pages/student';
@@ -11,14 +10,12 @@ import {
 } from './pages/faculty';
 import { StudentRoute, FacultyRoute } from './components/common';
 import { AuthProvider, NotificationProvider } from './context';
-import AuthCallback from './features/auth/components/AuthCallback';
 
 function AppContent() {
   return (
     <Routes>
       {/* Public Route */}
       <Route path="/" element={<LandingPage />} />
-      <Route path="/auth/callback" element={<AuthCallback />} />
 
       {/* Protected Student Routes */}
       <Route
@@ -78,21 +75,15 @@ function AppContent() {
   );
 }
 
-
 function App() {
-  // Replace with your actual Google Client ID from Google Cloud Console
-  const GOOGLE_CLIENT_ID = "860757277869-oncacfs23f3h40hhj0sliiq8chdo88cv.apps.googleusercontent.com";
-
   return (
-    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-      <Router>
-        <AuthProvider>
-          <NotificationProvider>
-            <AppContent />
-          </NotificationProvider>
-        </AuthProvider>
-      </Router>
-    </GoogleOAuthProvider>
+    <Router>
+      <AuthProvider>
+        <NotificationProvider>
+          <AppContent />
+        </NotificationProvider>
+      </AuthProvider>
+    </Router>
   );
 }
 
